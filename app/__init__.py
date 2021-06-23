d99071705d9184cad8939cc9ca72236331ab4e4c
Kibet 1998 <ronoronald1998@gmail.com>
2021-06-23 19:47:12 +0300

    Add post,comments and update account forms

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,10 @@ from flask import Flask
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_bootstrap import Bootstrap
 
+
+bootstrap = Bootstrap()
 db=SQLAlchemy()
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
@@ -14,6 +17,7 @@ def create_app(config_name):
 
   app.config.from_object(config_options[config_name])
   db.init_app(app)
+  bootstrap.init_app(app)
   from .main import main as main_blueprint
   app.register_blueprint(main_blueprint)
 
--- app/auth/views.py
+++ app/auth/views.py
@@ -2,11 +2,14 @@ from flask import render_template,redirect,url_for, flash,request
 from . import auth
 from ..models import User
 from .forms import RegistrationForm,LoginForm
-from flask_login import login_user,logout_user,login_required
+from flask_login import login_user,logout_user,login_required,current_user
 from .. import db
+
 # from ..email import mail_message
 @auth.route('/register',methods = ["GET","POST"])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('.index'))
     form = RegistrationForm()
     if form.validate_on_submit():
         user = User(email = form.email.data, username = form.username.data,password = form.password.data)
