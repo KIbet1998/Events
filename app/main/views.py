09e2b55b404a5d66fb6ecff4839bc54561c3d48e
Kibet 1998 <ronoronald1998@gmail.com>
2021-06-24 15:17:20 +0300

    Add create html file and its content

--- app/main/views.py
+++ app/main/views.py
@@ -3,7 +3,18 @@ from . import main
 
 @main.route('/')
 def index():
+  posts=Post.query.all()
   heading = 'Working.. Good to go'
   return render_template('index.html', heading=heading)
 
-  
+@main.route('/post/new',methods=['GET','POST'])
+@login_required
+def new_post():
+    form =PostForm()
+    if form.validate_on_submit():
+        post =Post(title=form.title.data,content=form.content.data,author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash("Your post has been created",'success')
+        return redirect(url_for('.home'))
+    return render_template('create_post.html',form=form)
